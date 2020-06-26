df8ae34ef9111e37edd5705023dfffe9975f1f4a
Raj2424 <s3634184@student.rmit.edu.au>
2020-06-27 03:28:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { fetchWeather } from './api/fetchWeather';
 import './App.css';
+import fampaylogo from './images/fampaylogo.png';
+import axios from "axios";
+import { Card, Icon, Image, Button } from 'semantic-ui-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  
+    return (
+      <div className="App">
+        
+        <img src={fampaylogo} className="App-logo" alt="fampaylogo" />
 
-export default App;
+       <br></br>
+       <br></br>
+       <br></br>
+
+       {/* First Card */}  
+
+    <Card className="HC1">
+      <Image 
+      height='81.2'
+      width='91'
+      src='https://westeros-staging.s3.amazonaws.com/media/images/generic/2a650f966e1f4a2e81bdbbb118fb2e73.png' 
+       />
+      <Card.Content>
+        <Card.Header>Matthew</Card.Header>
+        <Card.Meta>
+          <span className='date'>Joined in 2015</span>
+        </Card.Meta>
+        <Card.Description>
+          Matthew is a musician living in Nashville.
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          22 Friends
+        </a>
+      </Card.Content>
+   </Card>
+
+       
+
+
+    
+
+        
+      </div>
+    );
+  }
+  
+  export default App;
+  
